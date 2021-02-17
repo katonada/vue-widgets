c7d53dd16ac34f6fb93d232786472529d0f79ef9
Davor K 600 <Davor.Katona-Sesto@styria.hr>
2021-02-17 16:07:53 +0100

    serve api and index from other server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 
 const port = 8070;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
-app.use(express.static('server'))
-app.use(express.static('dist'))
+app.set('etag', false);
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store');
+    next();
+});
+app.use(express.static('server'));
+app.use(express.static('dist'));
